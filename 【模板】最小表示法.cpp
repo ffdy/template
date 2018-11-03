663dd015809b5d6f95dc4887bb6d2a004761b456
ffdy <1836017424@qq.com>
2018-11-03 08:19:06 +0800

    template

--- 【模板】最小表示法.cpp
+++ 【模板】最小表示法.cpp
@@ -5,25 +5,20 @@ using namespace std;
 const int maxn=3e5+10;
 int n,l;
 char str[maxn];
-int main()
-{
+int main() {
 	scanf("%d",&n);
-	while(n--)
-	{
+	while(n--) {
 		scanf("%s",str);
 		l=strlen(str);
 		int ll=0,rr=1;
-		while(ll<l&&rr<l)
-		{
+		while(ll<l&&rr<l) {
 			int i=0;
 			while(i<l&&str[(ll+i)%l]==str[(rr+i)%l]) i++;
 			if(i>=l) break;
-			if(str[(ll+i)%l]>str[(rr+i)%l]) 
-			{
+			if(str[(ll+i)%l]>str[(rr+i)%l]) {
 				ll=max(rr,ll+1+i);
 				rr=ll+1;
-			}
-			else rr=rr+i+1;
+			} else rr=rr+i+1;
 		}
 		printf("%d\n",ll);
 	}
